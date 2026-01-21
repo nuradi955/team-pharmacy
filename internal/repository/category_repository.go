0c1fdc4058f55178f940a8573a72e53fe46e472d
warcoder <warcoder@MacBook-Air-warcoder.local>
2026-01-21 17:24:55 +0300

    реализован category_repository

---
+++ internal/repository/category_repository.go
@@ -0,0 +1,52 @@
+package repository
+
+import (
+	"team-pharmacy/internal/models"
+
+	"gorm.io/gorm"
+)
+
+type CategoryRepository interface {
+	List() ([]models.Category, error)
+	Create(category *models.Category) error
+	GetByID(id uint) (*models.Category, error)
+
+	UpdateCategory(category *models.Category) error
+	DeleteCategory(id uint) error
+}
+
+type gormCategoryRepository struct {
+	db *gorm.DB
+}
+
+func NewCategoryRepository(db *gorm.DB) CategoryRepository {
+	return &gormCategoryRepository{db: db}
+}
+
+func (r *gormCategoryRepository) List() ([]models.Category, error) {
+	var categories []models.Category
+	if err := r.db.Find(&categories).Error; err != nil {
+		return nil, err
+	}
+	return categories, nil
+}
+
+func (r *gormCategoryRepository) Create(category *models.Category) error {
+	return r.db.Create(category).Error
+}
+
+func (r *gormCategoryRepository) GetByID(id uint) (*models.Category, error) {
+	var category models.Category
+	if err := r.db.First(&category, id).Error; err != nil {
+		return nil, err
+	}
+	return &category, nil
+}
+
+func (r *gormCategoryRepository) UpdateCategory(category *models.Category) error {
+	return r.db.Save(category).Error
+}
+
+func (r *gormCategoryRepository) DeleteCategory(id uint) error {
+	return r.db.Delete(&models.Category{}, id).Error
+}
