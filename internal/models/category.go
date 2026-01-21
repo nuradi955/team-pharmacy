68e488ae829a5540679d33d6644e0f598e09a546
warcoder <warcoder@MacBook-Air-warcoder.local>
2026-01-21 14:24:25 +0300

    add fix 2/ models category and subcategory

--- internal/models/category.go
+++ internal/models/category.go
@@ -7,5 +7,5 @@ import (
 type Category struct {
 	gorm.Model
 	Name          string        `gorm:"not null" json:"name"`
-	Subcategories []Subcategory `gorm:"foreignKey:CategoryID" json:"subcategories, omitempty"`
+	Subcategories []Subcategory `gorm:"foreignKey:CategoryID" json:"subcategories,omitempty"`
 }
--- internal/models/subcategory.go
+++ internal/models/subcategory.go
@@ -7,5 +7,5 @@ import (
 type Subcategory struct {
 	gorm.Model
 	Name       string `gorm:"not null" json:"name"`
-	CategoryID uint   `gorm:"not null" json:"category_id"`
+	CategoryID uint   `gorm:"constraint:OnDelete:CASCADE" json:"category_id"`
 }
