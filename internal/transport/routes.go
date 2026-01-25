13f337f30c04edba2dfe463f224d02170a62c058
warcoder <warcoder@example.com>
2026-01-26 02:40:08 +0300

    add routers/main

--- internal/transport/routes.go
+++ internal/transport/routes.go
@@ -9,15 +9,19 @@ import (
 func RegisterRoutes(router *gin.Engine,
 	userService services.UserService,
 	cartService services.CartService,
-	orderService services.OrderService) {
+	orderService services.OrderService,
+	categoryService services.CategoryService,
+	subcategoryService services.SubcategoryService) {
 
 	userHandler := NewUserHandler(userService)
 	categoryHandler := NewCategoryHandler(categoryService)
+	subcategoryHandler := NewSubcategoryHandler(subcategoryService)
 	cartHandler := NewCartHandler(cartService)
 	orderHandler := NewOrderHandler(orderService, userService, cartService)
 
 	userHandler.RegisterRoutes(router)
 	categoryHandler.RegisterRoutes(router)
+	subcategoryHandler.RegisterRoutes(router)
 	cartHandler.RegisterRoutes(router)
 	orderHandler.RegisterRoutes(router)
 
